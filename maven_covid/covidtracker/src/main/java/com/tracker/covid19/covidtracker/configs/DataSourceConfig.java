9a217182b0b1760d7cae138c19a5b2ae7bc982cb
Jawahar Shanmugam <jawaharvec@gmail.com>
2020-04-22 15:13:33 +0530

    added h2 database

--- maven_covid/covidtracker/src/main/java/com/tracker/covid19/covidtracker/configs/DataSourceConfig.java
+++ maven_covid/covidtracker/src/main/java/com/tracker/covid19/covidtracker/configs/DataSourceConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.tracker.covid19.covidtracker.configs;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -20,3 +21,4 @@ public class DataSourceConfig {
         return dataSourceBuilder.build();
     }
 }
+*/
